6039c2473a1964a32a222525c812b91c0cadc151
Angelica Mendes Coutinho <angelica.coutinho@estudante.ufjf.br>
2025-07-29 14:52:16 -0300

    adiciona disco

--- main.js
+++ main.js
@@ -3,9 +3,18 @@ eH1.textContent = "Olá mundo!";
 
 const eTabuleiro = criaTabuleiro();
 document.body.append(eTabuleiro);
+const disco1 = criaDisco("preto");
+eTabuleiro.append(disco1);
 
 function criaTabuleiro(){
     const eTabuleiro = document.createElement("div");
     eTabuleiro.classList.add("tabuleiro");
     return eTabuleiro;
+}
+
+function criaDisco(cor){
+    const novoDisco = document.createElement("div");
+    novoDisco.classList.add("disco");
+    novoDisco.dataset.cor = cor;
+    return novoDisco;
 }
